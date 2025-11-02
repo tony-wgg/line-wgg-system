fb0c6ca016376acb509f3a245ed325974e1da9e6
tony-wgg <tonysevan71@gmail.com>
2025-11-02 13:40:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from line-wgg-system!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
